fb5f4e4b6b2a6ae65cba8dba3715c419ed862f5d
psicmarlyslabecademarchan-star <psicmarlyslabecademarchan@gmail.com>
2026-09-02 20:51:51 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu'
+        ]
+    }
+});
+
+client.on('qr', (qr) => {
+    console.log('--- CÓDIGO QR GENERADO ---');
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('¡El bot está listo y funcionando!');
+});
+
+client.initialize();
